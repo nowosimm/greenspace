db9b74d800c79305470fc15c51a498310fd424db
Maddie <nowosimm@mail.uc.edu>
2024-10-22 09:24:02 -0400

    grabs all images in gallery

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,52 +1,51 @@
-import { ScrollArea } from "@mantine/core";
-
-import current from "./images/current.jpeg";
-import old from "./images/old.jpeg";
-import plant from "./images/plant.jpeg";
-import shelf from "./images/plantShelf.jpeg";
+import { ScrollArea , Center, Grid} from "@mantine/core";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "../css/Gallery.css"
 
 export default function () {
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    const callServer = async () => {
+      let response = await (
+        await fetch("http://localhost:3000/", {
+          credentials: "include",
+        })
+      ).json();
+      setPlants(response);
+    };
+    callServer();
+  }, []);
+
+  const gallery = [];
+  for (let i = 0; i < plants.length; i++) {
+    var plant = plants[i];
+    if (plant.picturePath) {
+      gallery.push({
+        _id: plant._id,
+        src: `http://localhost:3000/plant/${plant._id}/picture/${plant.picturePath}`,
+        type: plant.type,
+      });
+      console.log(plant.src);
+    }
+  }
   return (
     <div className="font-body">
-      <div className="font-semibold text-xl mb-10">Gallery</div>
       <ScrollArea h={1200}>
         <div className="grid grid-cols-3 gap-5 border-5 m-5">
-          <img
-            className="w-full aspect-square rounded-md hover:opacity-35"
-            src={old}
-          />
-          <img
-            className="w-full aspect-square rounded-md hover:opacity-35"
-            src={current}
-          />
-          <img
-            className="w-full aspect-square rounded-md hover:opacity-35"
-            src={plant}
-          />
-          <img
-            className="w-full aspect-square rounded-md hover:opacity-35"
-            src={plant}
-          />
-          <img
-            className="w-full aspect-square rounded-md hover:opacity-35"
-            src={old}
-          />
-          <img
-            className="w-full aspect-square rounded-md hover:opacity-35"
-            src={current}
-          />
-                  <img
-            className="w-full aspect-square rounded-md hover:opacity-35"
-            src={plant}
-          />
-          <img
-            className="w-full aspect-square rounded-md hover:opacity-35"
-            src={old}
-          />
-          <img
-            className="w-full aspect-square rounded-md hover:opacity-35"
-            src={current}
-          />
+          {gallery.map((p) => (
+            <Link to={`/plant/${p._id}`}>
+              <div className="container">
+                <img src={p.src} className="image"></img>
+                <div className="middle">
+                  <div className="text">
+                    {p.type}
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))}
         </div>
       </ScrollArea>
     </div>
